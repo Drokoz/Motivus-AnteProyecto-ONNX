b7262de7e2e89ba2b7f639083852d3ba1bc6437e
Tomás Herceg <49624623+Drokoz@users.noreply.github.com>
2023-09-04 12:24:34 -0300

    testing

--- public/scripts/modelRunner.js
+++ public/scripts/modelRunner.js
@@ -38,7 +38,7 @@ async function runBatchModel(
 ) {
   if (urlArray.length === 0) {
     console.log("Obteniendo arreglo");
-    urlArray = await fetch("http://localhost:3001/urlArray");
+    urlArray = urlArray.concat(await fetch("http://localhost:3001/urlArray"));
   }
   console.log("Arreglo obtenido: ");
   console.log(JSON.stringify(urlArray));
